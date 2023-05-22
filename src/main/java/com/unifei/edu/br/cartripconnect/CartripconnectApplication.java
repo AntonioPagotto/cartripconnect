5fe68d2d34997426f690c6783ec75ed719f70df8
pagotto <antoniopagotto121@gmail.com>
2023-05-22 12:21:09 -0300

    base project

---
+++ src/main/java/com/unifei/edu/br/cartripconnect/CartripconnectApplication.java
@@ -0,0 +1,13 @@
+package com.unifei.edu.br.cartripconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartripconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartripconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifei/edu/br/cartripconnect/CartripconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifei.edu.br.cartripconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartripconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
