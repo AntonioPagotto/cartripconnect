01173d79b352af7a22c387e74047a20d7fa589c8
Pagotto <antonio.pagotto@ab-inbev.com>
2023-05-30 11:25:00 -0300

    updates

---
+++ frontend/cartripconnect/src/app/cadastro-motorista/cadastro-motorista.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-motorista-component',
+  templateUrl: './cadastro-motorista.component.html',
+  styleUrls: ['./cadastro-motorista.component.css']
+})
+export class CadastroMotoristaComponent {
+}
---
+++ frontend/cartripconnect/src/app/cadastro-passageiro/cadastro-passageiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-passageiro-component',
+  templateUrl: './cadastro-passageiro.component.html',
+  styleUrls: ['./cadastro-passageiro.component.css']
+})
+export class CadastroPassageiroComponent {
+}
